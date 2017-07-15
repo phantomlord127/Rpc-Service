9a97f6b2e4fe53ef3b1cd8139a608ef472fb3c83
Timo Brinkmann <tb@trinkmann.com>
2017-07-15 17:01:18 +0200

    Sendet und empängt Data. Dazu auch Konvertierung vorhanden.

--- TB_RpcService/RpcServer.cs
+++ TB_RpcService/RpcServer.cs
@@ -122,12 +122,9 @@ namespace TB_RpcService
                 {
                     byte[] data = new byte[received]; //the data is in the byte[] format, not string!
                     Buffer.BlockCopy(_buffer, 0, data, 0, data.Length);
-                    //Console.WriteLine(Encoding.UTF8.GetString(data));
-                    foreach (EncodingInfo encoding in Encoding.GetEncodings())
-                    {
-                        Console.WriteLine($"{encoding.Name} ergibt: {encoding.GetEncoding().GetString(data)}");
-                    }
-                    client.Send(data);
+                    string msg = GetDecodedData(data, data.Length);
+                    Console.WriteLine($"Message: {msg}");
+                    client.Send(GetEncodeMessage("Hällo Wörld!"));
                     client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), client);
                 }
             }
@@ -151,6 +148,108 @@ namespace TB_RpcService
         private static byte[] ComputeHash(string str)
         {
             return sha1.ComputeHash(Encoding.ASCII.GetBytes(str));
+        }
+
+        private static byte[] GetEncodeMessage(string message)
+        {
+            Byte[] response;
+            Byte[] bytesRaw = Encoding.UTF8.GetBytes(message);
+            Byte[] frame = new Byte[10];
+
+            Int32 indexStartRawData = -1;
+            Int32 length = bytesRaw.Length;
+
+            frame[0] = (Byte)129;
+            if (length <= 125)
+            {
+                frame[1] = (Byte)length;
+                indexStartRawData = 2;
+            }
+            else if (length >= 126 && length <= 65535)
+            {
+                frame[1] = (Byte)126;
+                frame[2] = (Byte)((length >> 8) & 255);
+                frame[3] = (Byte)(length & 255);
+                indexStartRawData = 4;
+            }
+            else
+            {
+                frame[1] = (Byte)127;
+                frame[2] = (Byte)((length >> 56) & 255);
+                frame[3] = (Byte)((length >> 48) & 255);
+                frame[4] = (Byte)((length >> 40) & 255);
+                frame[5] = (Byte)((length >> 32) & 255);
+                frame[6] = (Byte)((length >> 24) & 255);
+                frame[7] = (Byte)((length >> 16) & 255);
+                frame[8] = (Byte)((length >> 8) & 255);
+                frame[9] = (Byte)(length & 255);
+
+                indexStartRawData = 10;
+            }
+
+            response = new Byte[indexStartRawData + length];
+
+            Int32 i, reponseIdx = 0;
+
+            //Add the frame bytes to the reponse
+            for (i = 0; i < indexStartRawData; i++)
+            {
+                response[reponseIdx] = frame[i];
+                reponseIdx++;
+            }
+
+            //Add the data bytes to the response
+            for (i = 0; i < length; i++)
+            {
+                response[reponseIdx] = bytesRaw[i];
+                reponseIdx++;
+            }
+
+            return response;
+        }
+
+        public static string GetDecodedData(byte[] buffer, int length)
+        {
+            byte b = buffer[1];
+            int dataLength = 0;
+            int totalLength = 0;
+            int keyIndex = 0;
+
+            if (b - 128 <= 125)
+            {
+                dataLength = b - 128;
+                keyIndex = 2;
+                totalLength = dataLength + 6;
+            }
+
+            if (b - 128 == 126)
+            {
+                dataLength = BitConverter.ToInt16(new byte[] { buffer[3], buffer[2] }, 0);
+                keyIndex = 4;
+                totalLength = dataLength + 8;
+            }
+
+            if (b - 128 == 127)
+            {
+                dataLength = (int)BitConverter.ToInt64(new byte[] { buffer[9], buffer[8], buffer[7], buffer[6], buffer[5], buffer[4], buffer[3], buffer[2] }, 0);
+                keyIndex = 10;
+                totalLength = dataLength + 14;
+            }
+
+            if (totalLength > length)
+                throw new Exception("The buffer length is small than the data length");
+
+            byte[] key = new byte[] { buffer[keyIndex], buffer[keyIndex + 1], buffer[keyIndex + 2], buffer[keyIndex + 3] };
+
+            int dataIndex = keyIndex + 4;
+            int count = 0;
+            for (int i = dataIndex; i < totalLength; i++)
+            {
+                buffer[i] = (byte)(buffer[i] ^ key[count % 4]);
+                count++;
+            }
+
+            return Encoding.UTF8.GetString(buffer, dataIndex, dataLength);
         }
     }
 
